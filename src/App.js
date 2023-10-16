7d62b15d6416c1128aaffd1bbe8c1911d66f960a
DanielComac <danielcomacxbox@gmail.com>
2023-10-16 10:43:02 -0600

    v4

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Contador from './components/Contador';
 import BarraSuperior from './components/BarraSuperior';
 import Cartas from './components/Cartas';
